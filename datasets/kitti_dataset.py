7edd233426523189cde568ffb042371eb5b7c72a
Fengyi Li <fengyi.li18@imperial.ac.uk>
2022-07-20 04:03:12 +0800

    style(deoth_deconder & kitti_dataset): Code style opti

--- datasets/kitti_dataset.py
+++ datasets/kitti_dataset.py
@@ -7,9 +7,10 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import skimage.transform
-import numpy as np
+
 import PIL.Image as pil
+import numpy as np
+import skimage.transform
 
 from kitti_utils import generate_depth_map
 from .mono_dataset import MonoDataset
@@ -18,6 +19,7 @@ from .mono_dataset import MonoDataset
 class KITTIDataset(MonoDataset):
     """Superclass for different types of KITTI dataset loaders
     """
+
     def __init__(self, *args, **kwargs):
         super(KITTIDataset, self).__init__(*args, **kwargs)
 
@@ -58,6 +60,7 @@ class KITTIDataset(MonoDataset):
 class KITTIRAWDataset(KITTIDataset):
     """KITTI dataset which loads the original velodyne depth maps for ground truth
     """
+
     def __init__(self, *args, **kwargs):
         super(KITTIRAWDataset, self).__init__(*args, **kwargs)
 
@@ -88,6 +91,7 @@ class KITTIRAWDataset(KITTIDataset):
 class KITTIOdomDataset(KITTIDataset):
     """KITTI dataset for odometry training and testing
     """
+
     def __init__(self, *args, **kwargs):
         super(KITTIOdomDataset, self).__init__(*args, **kwargs)
 
@@ -104,6 +108,7 @@ class KITTIOdomDataset(KITTIDataset):
 class KITTIDepthDataset(KITTIDataset):
     """KITTI dataset which uses the updated ground truth depth maps
     """
+
     def __init__(self, *args, **kwargs):
         super(KITTIDepthDataset, self).__init__(*args, **kwargs)
 
--- networks/depth_decoder.py
+++ networks/depth_decoder.py
@@ -6,11 +6,11 @@
 
 from __future__ import absolute_import, division, print_function
 
-import numpy as np
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 
-from collections import OrderedDict
 from layers import *
 
 
