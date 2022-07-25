61bc44277a6e91e129f16bb7b62fe428801301be
Fengyi Li <fengyi.li18@imperial.ac.uk>
2022-07-25 21:03:12 +0800

    fix(test_simple): generate predicted img

--- test_simple.py
+++ test_simple.py
@@ -28,7 +28,7 @@ def parse_args(weight_n=0, model_path=""):
 
     parser.add_argument('--image_path', type=str,
                         help='path to a test image or folder of images',
-                        default=r"masked_test_1_v/")
+                        default=r"masked_test_img/")
     parser.add_argument('--model_name', type=str,
                         help='name of a pretrained model to use',
                         choices=[
