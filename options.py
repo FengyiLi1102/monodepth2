3d0a29c58e532c5ed48a23d7ad187eed48f54347
Fengyi Li <fengyi.li18@imperial.ac.uk>
2022-08-10 04:17:07 +0800

    test(option): rendered, no masked

--- options.py
+++ options.py
@@ -97,7 +97,7 @@ class MonodepthOptions:
         self.parser.add_argument("--batch_size",
                                  type=int,
                                  help="batch size",
-                                 default=15)
+                                 default=12)
         self.parser.add_argument("--learning_rate",
                                  type=float,
                                  help="learning rate",
